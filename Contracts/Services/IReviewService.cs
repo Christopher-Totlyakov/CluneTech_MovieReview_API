6dbdcfc14c518c184a6208d3f59c93a1d8bf8429
Christopher Totlyakov <laminex0622@gmail.com>
2025-08-27 23:57:13 +0300

    add IReviewService

---
+++ Contracts/Services/IReviewService.cs
@@ -0,0 +1,17 @@
+﻿using Models.Review;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts.Services;
+
+/// <summary>
+/// Service contract for reviews.
+/// </summary>
+public interface IReviewService
+{
+    Task<IEnumerable<ReviewDto>> GetReviewsForMovieAsync(int movieId);
+    Task<ReviewDto> CreateReviewAsync(string userId, CreateReviewDto dto);
+}
