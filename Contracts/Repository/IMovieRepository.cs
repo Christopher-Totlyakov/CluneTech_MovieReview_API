9f9462d636eceddf94ee77541b151d1c69043e1f
Christopher Totlyakov <laminex0622@gmail.com>
2025-08-27 23:53:28 +0300

    add IMovieRepository

---
+++ Contracts/Repository/IMovieRepository.cs
@@ -0,0 +1,21 @@
+﻿using Contracts.Repository.Base;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts.Repository;
+
+/// <summary>
+/// Defines repository methods for working with movies.
+/// </summary>
+public interface IMovieRepository : IRepositoryBase<Movie>
+{
+    /// <summary>
+    /// Gets a movie including its reviews by ID.
+    /// </summary>
+    Task<Movie?> GetMovieWithReviewsAsync(int movieId);
+}
+
