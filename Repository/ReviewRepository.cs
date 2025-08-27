d7a6b7496894d4eb33371dee21f4c3ddbca07d6e
Christopher Totlyakov <laminex0622@gmail.com>
2025-08-27 23:54:24 +0300

    add ReviewRepository

---
+++ Repository/ReviewRepository.cs
@@ -0,0 +1,31 @@
+﻿using Contracts.Repository;
+using Entities;
+using Repository.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository;
+
+/// <summary>
+/// Repository for review management.
+/// </summary>
+public class ReviewRepository : RepositoryBase<Review>, IReviewRepository
+{
+    protected readonly RepositoryContext _context;
+    public ReviewRepository(RepositoryContext context) : base(context) 
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Review>> GetReviewsForMovieAsync(int movieId)
+    {
+        return await _context.Reviews
+            .Where(r => r.MovieId == movieId)
+            .Include(r => r.User)
+            .ToListAsync();
+    }
+}
