309716bfff93c1a7b7f2297fe4dd5e11c8a5adc7
Christopher Totlyakov <laminex0622@gmail.com>
2025-08-27 23:55:40 +0300

    add Review DTOs

---
+++ Models/Review/CreateReviewDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Review;
+
+
+/// <summary>
+/// DTO for creating a review.
+/// </summary>
+public class CreateReviewDto
+{
+    public string Comment { get; set; } = string.Empty;
+
+    public int Rating { get; set; }
+
+    public int MovieId { get; set; }
+}
---
+++ Models/Review/ReviewDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Review;
+
+/// <summary>
+/// Data Transfer Object for reviews.
+/// </summary>
+public class ReviewDto
+{
+    public int Id { get; set; }
+    public string Comment { get; set; } = string.Empty;
+    public int Rating { get; set; }
+    public string UserName { get; set; } = string.Empty;
+    public int MovieId { get; set; }
+}
