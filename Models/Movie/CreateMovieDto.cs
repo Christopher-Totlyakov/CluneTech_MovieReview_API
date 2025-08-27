ea61b0e5f00e3f148c3ac35f3daeb403f0a832da
Christopher Totlyakov <laminex0622@gmail.com>
2025-08-27 23:55:04 +0300

    add movie DTOs

---
+++ Models/Movie/CreateMovieDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Movie;
+
+
+/// <summary>
+/// DTO for creating a movie.
+/// </summary>
+public class CreateMovieDto
+{
+    public string Title { get; set; } = string.Empty;
+
+    public string? Description { get; set; }
+
+    public DateTime ReleaseDate { get; set; }
+}
---
+++ Models/Movie/MovieDto.cs
@@ -0,0 +1,21 @@
+﻿using Models.Review;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Movie;
+
+/// <summary>
+/// Data Transfer Object for movies.
+/// </summary>
+public class MovieDto
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public DateTime ReleaseDate { get; set; }
+
+    public IEnumerable<ReviewDto> Reviews { get; set; } = new List<ReviewDto>();
+}
