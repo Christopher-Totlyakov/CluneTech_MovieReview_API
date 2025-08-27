3265335306bfa392afab771bc9c1fd3d79594cee
Christopher Totlyakov <laminex0622@gmail.com>
2025-08-27 23:53:43 +0300

    add IReviewRepository

---
+++ Contracts/Repository/IReviewRepository.cs
@@ -0,0 +1,20 @@
+﻿using Contracts.Repository.Base;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts.Repository;
+
+/// <summary>
+/// Defines repository methods for working with reviews.
+/// </summary>
+public interface IReviewRepository : IRepositoryBase<Review>
+{
+    /// <summary>
+    /// Gets all reviews for a specific movie.
+    /// </summary>
+    Task<IEnumerable<Review>> GetReviewsForMovieAsync(int movieId);
+}
