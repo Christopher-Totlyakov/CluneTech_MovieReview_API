9b5e19d6ddd7c6ef9d26b6d8a0fadba7d6ee17b0
Christopher Totlyakov <laminex0622@gmail.com>
2025-08-27 23:56:21 +0300

    add IMovieService

---
+++ Contracts/Services/IMovieService.cs
@@ -0,0 +1,20 @@
+﻿using Models.Movie;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts.Services;
+
+/// <summary>
+/// Service contract for movies.
+/// </summary>
+public interface IMovieService
+{
+    Task<IEnumerable<MovieDto>> GetAllMoviesAsync();
+    Task<MovieDto?> GetMovieByIdAsync(int id);
+    Task<MovieDto> CreateMovieAsync(CreateMovieDto dto);
+    Task<bool> UpdateMovieAsync(int id, CreateMovieDto dto);
+    Task<bool> DeleteMovieAsync(int id);
+}
--- MovieReview/Configuration/ServicesConfiguration.cs
+++ MovieReview/Configuration/ServicesConfiguration.cs
@@ -1,7 +1,9 @@
 ﻿using Contracts.Repository;
+using Contracts.Services;
 using Entities;
 using Microsoft.AspNetCore.Identity;
 using Repository;
+using Services;
 using System.ComponentModel.Design;
 
 namespace MovieReview.Configuration;
@@ -10,6 +12,10 @@ public static class ServicesConfiguration
 {
     public static void ConfigureServices(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddScoped<IMovieRepository, MovieRepository>();
+        services.AddScoped<IReviewRepository, ReviewRepository>();
 
+        services.AddScoped<IMovieService, MovieService>();
+        services.AddScoped<IReviewService, ReviewService>();
     }
 }
