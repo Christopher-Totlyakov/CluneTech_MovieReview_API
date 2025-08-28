4e63a83b8e89961142a9d3813eecca41b809f4db
Christopher Totlyakov <laminex0622@gmail.com>
2025-08-28 21:15:28 +0300

    add Auth DTOs

---
+++ Models/Auth/AuthResponseDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Auth;
+
+public class AuthResponseDto
+{
+    public string UserId { get; set; } = null!;
+    public string UserName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Token { get; set; } = null!;
+}
---
+++ Models/Auth/RegisterDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Auth;
+public class RegisterDto
+{
+    public string UserName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Password { get; set; } = null!;
+}
