abbf5991c1c3869f76b5e40a54ea4f7b1bdaf44b
Christopher Totlyakov <laminex0622@gmail.com>
2025-08-30 17:27:08 +0300

    add ISeriesService

---
+++ Contracts/Services/ISeriesService.cs
@@ -0,0 +1,29 @@
+﻿using Models.Series;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts.Services;
+
+/// <summary>
+/// Service contract for TV series.
+/// </summary>
+public interface ISeriesService
+{
+    Task<IEnumerable<SeriesDto>> GetAllSeriesAsync();
+    Task<SeriesDto?> GetSeriesByIdAsync(long id);
+    Task<SeriesDto> CreateSeriesAsync(CreateSeriesDto dto);
+    Task<bool> UpdateSeriesAsync(long id, CreateSeriesDto dto);
+    Task<bool> DeleteSeriesAsync(long id);
+
+    Task<SeasonDto?> AddSeasonAsync(long seriesId, CreateSeasonDto dto);
+    Task<bool> UpdateSeasonAsync(long seasonId, CreateSeasonDto dto);
+    Task<bool> DeleteSeasonAsync(long seasonId);
+
+
+    Task<EpisodeDto?> AddEpisodeAsync(long seasonId, CreateEpisodeDto dto);
+    Task<bool> UpdateEpisodeAsync(long episodeId, CreateEpisodeDto dto);
+    Task<bool> DeleteEpisodeAsync(long episodeId);
+}
