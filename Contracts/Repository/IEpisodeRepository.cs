67d38cd9e2b4abb52edc8d0e9c35a9cc6e77c6f2
Christopher Totlyakov <laminex0622@gmail.com>
2025-08-30 17:25:45 +0300

    add IEpisodeRepository

---
+++ Contracts/Repository/IEpisodeRepository.cs
@@ -0,0 +1,14 @@
+﻿using Contracts.Repository.Base;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts.Repository;
+
+public interface IEpisodeRepository : IRepositoryBase<Episode>
+{
+    Task<IEnumerable<Episode>> GetBySeasonIdAsync(long seasonId);
+}
