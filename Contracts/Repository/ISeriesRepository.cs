fb67a087d018d8fe4385af97224f81becdbe3385
Christopher Totlyakov <laminex0622@gmail.com>
2025-08-30 17:25:27 +0300

    add ISeriesRepository

---
+++ Contracts/Repository/ISeriesRepository.cs
@@ -0,0 +1,15 @@
+﻿using Contracts.Repository.Base;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts.Repository;
+
+public interface ISeriesRepository : IRepositoryBase<Series>
+{
+    Task<Series?> GetSeriesWithReviewsAsync(long id);
+    Task<Series?> GetSeriesWithSeasonsAndEpisodesAsync(long id);
+}
